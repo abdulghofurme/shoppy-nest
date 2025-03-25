4afaf3bd7e08853d9d59106d7d9143159f4ee19a
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-03-25 15:24:36 +0700

    refactor(http-exception): Improve error response structure and handling
    - Enhanced HttpExceptionFilter to provide a more structured error response.
    - Consolidated error handling for Prisma and NestJS exceptions.
    - Added support for detailed error messages and field-specific error arrays.
    - Updated response format to include statusCode, message, and errors for better client-side handling.

--- src/http-exception/http-exception.filter.ts
+++ src/http-exception/http-exception.filter.ts
@@ -8,45 +8,68 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse<Response>()
 
-    let status = HttpStatus.INTERNAL_SERVER_ERROR
-    let message = 'Internal server error'
+    let errorResponse: Record<string, unknown> & { statusCode: number, message: string } = {
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal server error'
+    }
 
     if (exception instanceof Prisma.PrismaClientKnownRequestError) {
       // Handle Prisma Errors
       switch (exception.code) {
         case 'P2002':
-          status = HttpStatus.CONFLICT
-          message = `Unique constraint failed on ${exception.meta?.target}`;
+          errorResponse.statusCode = HttpStatus.CONFLICT
+          errorResponse.message = `Unique constraint failed`;
           break;
         case 'P2025':
-          status = HttpStatus.NOT_FOUND;
-          message = 'Record not found';
+          errorResponse.statusCode = HttpStatus.NOT_FOUND;
+          errorResponse.message = 'Record not found';
           break;
         case 'P2003':
-          status = HttpStatus.BAD_REQUEST;
-          message = 'Foreign key constraint failed';
+          errorResponse.statusCode = HttpStatus.BAD_REQUEST;
+          errorResponse.message = 'Foreign key constraint failed';
           break;
         case 'P2014':
-          status = HttpStatus.BAD_REQUEST;
-          message = 'Invalid cascade delete operation';
+          errorResponse.statusCode = HttpStatus.BAD_REQUEST;
+          errorResponse.message = 'Invalid cascade delete operation';
           break;
         default:
-          message = 'Databse error'
+          errorResponse.message = 'Databse error'
       }
     } else if (exception instanceof HttpException) {
       // Handle NestJS HttpException
-      status = exception.getStatus();
+      errorResponse.statusCode = exception.getStatus();
       const exceptionResponse = exception.getResponse()
-      if (typeof exceptionResponse === 'string') {
-        message = exceptionResponse
+
+      if (typeof exceptionResponse === 'object') {
+        if ('message' in exceptionResponse) {
+          const messages = exceptionResponse['message'];
+
+          let errors: Record<string, string[]> = {};
+          if (Array.isArray(messages)) {
+            // Mengonversi pesan menjadi key-value
+            messages.forEach((msg: string) => {
+              const parts = msg.split(' '); // Pisahkan kata-kata pesan
+              const field = parts[0].toLowerCase(); // Ambil kata pertama sebagai field (biasanya nama field)
+
+              if (!errors[field]) {
+                errors[field] = [];
+              }
+              errors[field].push(msg); // Tambahkan pesan ke array field terkait
+            });
+          } else if (typeof messages === 'string') {
+            errors = { general: [messages] };
+            errorResponse.message = messages
+          }
+          errorResponse.errors = errors
+        }
+        if ('error' in exceptionResponse && typeof exceptionResponse.error === 'string') {
+          errorResponse.message = exceptionResponse.error
+        }
       } else {
-        message = JSON.stringify(exception.getResponse())
+        errorResponse.message = exceptionResponse
       }
     }
 
-    response.status(status).json({
-      statusCode: status,
-      message,
-    });
+    response.status(errorResponse.statusCode).json(errorResponse);
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { CreateUserRequestDto } from './dto/create-user.request';
 import { UsersService } from './users.service';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) { }
 
 	@Post()
+	@UseInterceptors(NoFilesInterceptor())
 	createUser(@Body() payload: CreateUserRequestDto) {
 		return this.usersService.createUser(payload)
 	}
