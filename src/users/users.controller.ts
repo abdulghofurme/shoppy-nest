8e67daddce9e0f15307fee00e4f0a54381d4e4bd
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-03-25 11:50:25 +0700

    feat(users): Implement user creation with validation and password hashing
    - Added bcrypt for password hashing.
    - Introduced class-transformer and class-validator for DTO transformation and validation.
    - Updated UsersController to handle user creation requests.
    - Enhanced UsersService to create users with hashed passwords using Prisma.
    - Configured global validation pipes in main.ts for automatic DTO transformation and property whitelisting.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequestDto } from './dto/create-user.request';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	@Post()
+	createUser(@Body() payload: CreateUserRequestDto) {
+		
+	}
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
+import { CreateUserRequestDto } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(private readonly prismaService: PrismaService) { }
+
+	async createUser(payload: CreateUserRequestDto): Promise<User> {
+		return await this.prismaService.user.create({
+			data: {
+				...payload,
+				password: await bcrypt.hasch(payload.password, 10)
+			}
+		})
+	}
+}
