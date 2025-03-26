3c1b0c27072785a45cfa84bb1f9119c7fe22209e
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-03-26 11:49:50 +0700

    feat(auth): Sign JWT - integrate JWT authentication and enhance login functionality
    - Added @nestjs/jwt for JWT support in authentication.
    - Updated AuthModule to configure JWT with secret and expiration options.
    - Enhanced AuthController to handle user login and return JWT in response.
    - Refactored AuthService to include login method for generating and setting JWT as a cookie.
    - Updated package.json and package-lock.json to include new dependencies.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule]
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow("JWT_EXPIRATION")
+        }
+      }),
+      inject: [ConfigService]
+    }),
+    UsersModule,
+    ConfigModule
+  ]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,52 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt'
+import { Response } from 'express';
+import ms, { StringValue } from 'ms';
 import { UsersService } from 'src/users/users.service';
+import { TTokenPayload } from './token-payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-	constructor(private readonly usersService: UsersService) {}
+	constructor(
+		private readonly usersService: UsersService,
+		private readonly configService: ConfigService,
+		private readonly jwtService: JwtService,
+	) { }
 
 	async verifyUser(email: string, password: string) {
 		try {
-			const user = await this.usersService.getUser({email})
+			const user = await this.usersService.getUser({ email })
 			const authenticated = await bcrypt.compare(password, user.password)
 			if (!authenticated) {
 				throw new UnauthorizedException()
 			}
 		} catch (error) {
-			throw new UnauthorizedException('Credentials are not valid.')	
+			throw new UnauthorizedException('Credentials are not valid.')
 		}
+	}
+
+	async login(user: User, response: Response) {
+		const expires = new Date()
+		expires.setMilliseconds(
+			expires.getMilliseconds() + ms(
+				this.configService.getOrThrow<StringValue>('JWT_EXPIRATION')
+			)
+		)
+
+		const tokenPayload: TTokenPayload = {
+			userId: user.id
+		}
+		const token = this.jwtService.sign(tokenPayload)
+
+		response.cookie('Authentication', token, {
+			secure: true,
+			httpOnly: true,
+			expires,
+		})
+
+		return tokenPayload
 	}
 }
