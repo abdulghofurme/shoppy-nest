a6c845775c4edb6c91fcbe8cccf2ec24afe56a3e
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-04-11 13:19:25 +0700

    refactor(app): remove unused PrismaModule import from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
-import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { ImagesModule } from './images/images.module';
